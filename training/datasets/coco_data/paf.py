86a3303746c0216998b95fee8a0456a13486eb8c
Ricardo Rossiter Barioni <rrb@cin.ufpe.br>
2018-11-29 09:54:04 -0300

    paf.py: print vec_map[yy, xx] shape and mask[:,:, np.newaxis] shape

--- training/datasets/coco_data/paf.py
+++ training/datasets/coco_data/paf.py
@@ -51,10 +51,8 @@ def putVecMaps(centerA, centerB, accumulate_vec_map, count, params_transform):
     mask = limb_width < thre  # mask is 2D
 
     vec_map = np.copy(accumulate_vec_map) * 0.0
-    print(vec_map[yy, xx])
-    xx = xx.flatten()
-    yy = yy.flatten()
-    print(vec_map[yy, xx])
+    print('vec_map[yy, xx].shape: ' + str(vec_map[yy, xx].shape))
+    print('mask[:, :, np.newaxis].shape: ' + str(mask[:, :, np.newaxis].shape))
     vec_map[yy, xx] = np.repeat(mask[:, :, np.newaxis], 2, axis=2)
     vec_map[yy, xx] *= limb_vec_unit[np.newaxis, np.newaxis, :]
 
